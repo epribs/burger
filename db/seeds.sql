34b304a0112796121d093789653a25a8c0a38b03
epribs <ericpribramsky@gmail.com>
2017-04-29 22:45:42 -0500

    Adding db files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Jim Ham-n-Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Manchego Scott Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Pam-Fried Egg and Bacon Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('D-White Cheddar and Sriracha Burger', false);
