eef724a49b442e08fd4f3a9954e51cb0884c7d12
epribs <ericpribramsky@gmail.com>
2017-04-29 22:56:40 -0500

    adding models

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVal, condition, cb) {
+    orm.update("burgers", objColVal, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
